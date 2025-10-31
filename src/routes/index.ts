bf36004d2a926ce1098835d8a1edea41f4fc83cf
rizkysafdila <rizkysafdila@gmail.com>
2025-10-31 22:22:26 +0700

    feat: add simple welcome page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,14 @@ import profileRouter from "./profile.route"
 import bannerRouter from "./banner.routes"
 import serviceRouter from "./service.routes"
 import transactionRouter from "./transaction.routes"
-import path from "path"
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'))
+  res.json({
+    message: 'Welcome to API SIMS PPOB v1 🚀',
+    developed_by: 'Muhammad Rizky Safdila',
+  })
 })
 
 router.use(authRoutes)
